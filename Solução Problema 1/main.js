7e750a0e879f88a9c243cc3d94f86d167b9d34ca
mc-gomes <bigmath2001@hotmail.com>
2022-02-04 13:43:33 -0300

    remoção de valores repetidos

--- Solução Problema 1/main.js
+++ Solução Problema 1/main.js
@@ -2,12 +2,19 @@ function ordenaNumeros(a, b) {
     return a - b
 }
 
+function removeRepetido(lista) {
+    return lista.filter(function(valor, pos, array) {
+        return !pos || valor != array[pos - 1]
+    })
+}
 
-console.log("--- Manipulando listas --- ")
 
-var listaNumeros = [12, 5, 1, 4, 7, 0, 12, 35, 21, 10, 4]
+console.log("--- Manipulando listas --- \n")
+
+var listaNumeros = [12, 5, 1, 4, 7, 0, 12, 35, 21, 150, 10, 4]
 console.log("Antes: ", listaNumeros)
 
 var listaNumAux = listaNumeros.sort(ordenaNumeros)
-console.log("Depois: ", listaNumAux)
 
+var listaSemRep = removeRepetido(listaNumAux)
+console.log("Depois: ", listaSemRep)
