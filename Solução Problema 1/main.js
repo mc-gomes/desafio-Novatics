59add8373a07c52973d615e5be38c9f15b97730c
mc-gomes <bigmath2001@hotmail.com>
2022-02-06 11:41:56 -0300

    adição de função que inicia a manipulação

--- Solução Problema 1/main.js
+++ Solução Problema 1/main.js
@@ -3,18 +3,26 @@ function ordenaNumeros(a, b) {
 }
 
 function removeRepetido(lista) {
-    return lista.filter(function(valor, pos, array) {
+    return lista.filter(function (valor, pos, array) {
         return !pos || valor != array[pos - 1]
     })
 }
 
+function manipulacao(lista) {
+    var listaNumAux = []
+    var listaSemRep = []
 
-console.log("--- Manipulando listas --- \n")
+    console.log("\nExemplo:")
+    console.log("Antes: ", lista)
 
-var listaNumeros = [12, 5, 1, 4, 7, 0, 12, 35, 21, 150, 10, 4]
-console.log("Antes: ", listaNumeros)
+    listaNumAux = lista.sort(ordenaNumeros)
+    listaSemRep = removeRepetido(listaNumAux)
 
-var listaNumAux = listaNumeros.sort(ordenaNumeros)
+    console.log("Depois: ", listaSemRep)
+}
 
-var listaSemRep = removeRepetido(listaNumAux)
-console.log("Depois: ", listaSemRep)
+console.log("--- Manipulando listas --- ")
+
+var listaNUmeros = [12, 5, 1, 4, 7, 0, 12, 35, 21, 150, 10, 4]
+manipulacao(listaNUmeros)
+
