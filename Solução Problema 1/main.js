f47fa6265f50b8ffdeac5f116abc6cc8257cf52e
mc-gomes <bigmath2001@hotmail.com>
2022-02-06 11:44:34 -0300

    adição de mais 2 listas de exemplo

--- Solução Problema 1/main.js
+++ Solução Problema 1/main.js
@@ -23,6 +23,11 @@ function manipulacao(lista) {
 
 console.log("--- Manipulando listas --- ")
 
-var listaNUmeros = [12, 5, 1, 4, 7, 0, 12, 35, 21, 150, 10, 4]
-manipulacao(listaNUmeros)
+var lista1 = [12, 5, 1, 4, 7, 0, 12, 35, 21, 150, 10, 4]
+manipulacao(lista1)
 
+var lista2 = [8, 5, 10, 5, 2, 4, 4, 3]
+manipulacao(lista2)
+
+var lista3 = [8, 8, 7, 7, 1, 6, 5, 5, 3, 1]
+manipulacao(lista3)
